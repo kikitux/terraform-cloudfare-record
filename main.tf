789838ea71d6bcba05436effa8cf51a93c5989d8
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-02-04 10:40:21 +0100

    add priority

--- main.tf
+++ main.tf
@@ -4,15 +4,20 @@ variable "record" {
   type = "map"
 }
 
+variable "priotity" {
+  default = 0
+}
+
 variable "type" {}
 variable "ttl" {}
 
 # Add a record to the domain
 resource "cloudflare_record" "record" {
-  count  = "${length(var.record)}"
-  domain = "${var.domain}"
-  name   = "${element(keys(var.record), count.index)}"
-  value  = "${element(values(var.record), count.index)}"
-  type   = "${var.type}"
-  ttl    = "${var.ttl}"
+  count   = "${length(var.record)}"
+  domain  = "${var.domain}"
+  name    = "${element(keys(var.record), count.index)}"
+  priotiy = "${var.priority}"
+  ttl     = "${var.ttl}"
+  type    = "${var.type}"
+  value   = "${element(values(var.record), count.index)}"
 }
