def02ad4473e9f2a66b5047cbb963b47f25632e1
Alvaro Miranda Aguilera <kikitux@gmail.com>
2020-03-09 15:16:45 +0100

    update

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 variable "domain" {}
 
 variable "record" {
-  type = "map"
+  type = map
 }
 
 variable "priority" {
@@ -13,11 +13,11 @@ variable "ttl" {}
 
 # Add a record to the domain
 resource "cloudflare_record" "record" {
-  count    = "${length(var.record)}"
-  domain   = "${var.domain}"
-  name     = "${element(keys(var.record), count.index)}"
-  priority = "${var.priority}"
-  ttl      = "${var.ttl}"
-  type     = "${var.type}"
-  value    = "${element(values(var.record), count.index)}"
+  count    = length(var.record)
+  domain   = var.domain
+  name     = element(keys(var.record), count.index)
+  priority = var.priority
+  ttl      = var.ttl
+  type     = var.type
+  value    = element(values(var.record), count.index)
 }
