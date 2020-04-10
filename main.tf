6688752d2b8ee56e2532f91eab63ceaf6f01820e
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2020-04-10 15:39:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ variable "ttl" {}
 
 # Add a record to the domain
 resource "cloudflare_record" "record" {
-  count    = length(var.record)
   domain   = var.domain
-  name     = element(keys(var.record), count.index)
   priority = var.priority
   ttl      = var.ttl
   type     = var.type
-  value    = element(values(var.record), count.index)
+  for_each = var.record
+  name     = each.key
+  value    = each.value
 }
